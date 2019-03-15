73585392cd9fc29b65f47b9a0ce5e39f0676832a
marcinbodnardesktop <me@marcinbodnar.com>
2019-03-15 23:58:05 +0100

    Updates

---
+++ precache-manifest.1f7738b24e6cbfdbb83477a3108815d4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "883a03a5a745bffe28e6",
+    "url": "/near-debugger-ui/static/css/main.a8e1208a.chunk.css"
+  },
+  {
+    "revision": "883a03a5a745bffe28e6",
+    "url": "/near-debugger-ui/static/js/main.883a03a5.chunk.js"
+  },
+  {
+    "revision": "d6b2a39b7404e581b95e",
+    "url": "/near-debugger-ui/static/js/1.d6b2a39b.chunk.js"
+  },
+  {
+    "revision": "c33aa8accacea69a671d",
+    "url": "/near-debugger-ui/static/js/runtime~main.c33aa8ac.js"
+  },
+  {
+    "revision": "0e5b384b03b5eba05a6d888a4b7742ef",
+    "url": "/near-debugger-ui/static/media/icon-search.0e5b384b.svg"
+  },
+  {
+    "revision": "dde73b38b35929e873bb5c9955f5c863",
+    "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Light.dde73b38.otf"
+  },
+  {
+    "revision": "971c1fec19ca99196578f34e66d5f0e9",
+    "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Medium.971c1fec.otf"
+  },
+  {
+    "revision": "6de0530b5b9708aa0ee264851a0cf1a2",
+    "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Regular.6de0530b.otf"
+  },
+  {
+    "revision": "b48f419e9385956be8212f284cafca0c",
+    "url": "/near-debugger-ui/static/media/icon-transactions.b48f419e.svg"
+  },
+  {
+    "revision": "81b24679c57aff845cf6646d8f914bef",
+    "url": "/near-debugger-ui/static/media/icon-blocks.81b24679.svg"
+  },
+  {
+    "revision": "52ab55a96c156e7c11f6dc10a2d9b7ef",
+    "url": "/near-debugger-ui/static/media/explorer-logo.52ab55a9.svg"
+  },
+  {
+    "revision": "1418a50be33270f87fe5fcdbca868e59",
+    "url": "/near-debugger-ui/static/media/icon-home.1418a50b.svg"
+  },
+  {
+    "revision": "de33aecefff6fcb359777b6c90e1a4a5",
+    "url": "/near-debugger-ui/static/media/icon-help.de33aece.svg"
+  },
+  {
+    "revision": "978ff55ad1565db9df99e8c89c6283dd",
+    "url": "/near-debugger-ui/static/media/icon-issues.978ff55a.svg"
+  },
+  {
+    "revision": "bc1e323684fd292186c9d2b7f7df89f6",
+    "url": "/near-debugger-ui/static/media/icon-m-transaction.bc1e3236.svg"
+  },
+  {
+    "revision": "e4ef464f370705a7b8f3b08b5af3edbd",
+    "url": "/near-debugger-ui/static/media/icon-m-receipt.e4ef464f.svg"
+  },
+  {
+    "revision": "581cd4397d6c47f1d86496e08b98d6dc",
+    "url": "/near-debugger-ui/static/media/icon-m-doc.581cd439.svg"
+  },
+  {
+    "revision": "b6da09110d7e31f72810cec96d57097f",
+    "url": "/near-debugger-ui/static/media/icon-account.b6da0911.svg"
+  },
+  {
+    "revision": "782a78a39e414addeafc06ecdef0a69e",
+    "url": "/near-debugger-ui/static/media/icon-contacts.782a78a3.svg"
+  },
+  {
+    "revision": "584de59e994c77e8745a3580caa0977b",
+    "url": "/near-debugger-ui/static/media/near.584de59e.svg"
+  },
+  {
+    "revision": "3f2f5a0c3d2d0d01ae456041a195e14b",
+    "url": "/near-debugger-ui/static/media/need-help-footer.3f2f5a0c.png"
+  },
+  {
+    "revision": "836b5f4124eea81f9097cd09a73edc54",
+    "url": "/near-debugger-ui/index.html"
+  }
+];
