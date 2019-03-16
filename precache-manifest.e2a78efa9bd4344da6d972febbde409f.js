dee7c6c27860bbb6d3aa3868abd392c4aa078236
marcinbodnardesktop <me@marcinbodnar.com>
2019-03-16 01:13:37 +0100

    Updates

--- precache-manifest.e2a78efa9bd4344da6d972febbde409f.js
+++ precache-manifest.e2a78efa9bd4344da6d972febbde409f.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "d36a1186421321c0998b",
-    "url": "/near-debugger-ui/static/css/main.a8e1208a.chunk.css"
+    "revision": "0a6b19557d390a58f49a",
+    "url": "/near-debugger-ui/static/css/main.7da1061d.chunk.css"
   },
   {
-    "revision": "d36a1186421321c0998b",
-    "url": "/near-debugger-ui/static/js/main.d36a1186.chunk.js"
+    "revision": "0a6b19557d390a58f49a",
+    "url": "/near-debugger-ui/static/js/main.0a6b1955.chunk.js"
   },
   {
-    "revision": "d6b2a39b7404e581b95e",
-    "url": "/near-debugger-ui/static/js/1.d6b2a39b.chunk.js"
+    "revision": "4d7ddf16817b2a983817",
+    "url": "/near-debugger-ui/static/js/1.4d7ddf16.chunk.js"
   },
   {
     "revision": "c33aa8accacea69a671d",
@@ -24,12 +24,12 @@ self.__precacheManifest = [
     "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Medium.971c1fec.otf"
   },
   {
-    "revision": "dde73b38b35929e873bb5c9955f5c863",
-    "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Light.dde73b38.otf"
-  },
-  {
     "revision": "6de0530b5b9708aa0ee264851a0cf1a2",
     "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Regular.6de0530b.otf"
+  },
+  {
+    "revision": "dde73b38b35929e873bb5c9955f5c863",
+    "url": "/near-debugger-ui/static/media/Branding with Type - Bw Seido Round Light.dde73b38.otf"
   },
   {
     "revision": "b48f419e9385956be8212f284cafca0c",
@@ -68,14 +68,6 @@ self.__precacheManifest = [
     "url": "/near-debugger-ui/static/media/icon-m-doc.581cd439.svg"
   },
   {
-    "revision": "b6da09110d7e31f72810cec96d57097f",
-    "url": "/near-debugger-ui/static/media/icon-account.b6da0911.svg"
-  },
-  {
-    "revision": "782a78a39e414addeafc06ecdef0a69e",
-    "url": "/near-debugger-ui/static/media/icon-contacts.782a78a3.svg"
-  },
-  {
     "revision": "584de59e994c77e8745a3580caa0977b",
     "url": "/near-debugger-ui/static/media/near.584de59e.svg"
   },
@@ -84,7 +76,15 @@ self.__precacheManifest = [
     "url": "/near-debugger-ui/static/media/need-help-footer.3f2f5a0c.png"
   },
   {
-    "revision": "fc718117ae2ebe270e97ada9a59d0684",
+    "revision": "b6da09110d7e31f72810cec96d57097f",
+    "url": "/near-debugger-ui/static/media/icon-account.b6da0911.svg"
+  },
+  {
+    "revision": "782a78a39e414addeafc06ecdef0a69e",
+    "url": "/near-debugger-ui/static/media/icon-contacts.782a78a3.svg"
+  },
+  {
+    "revision": "e1779fb947f2658256150fb61235085b",
     "url": "/near-debugger-ui/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/near-debugger-ui/precache-manifest.e304788760d75566c5c7eefc67cd4987.js"
+  "/near-debugger-ui/precache-manifest.e2a78efa9bd4344da6d972febbde409f.js"
 );
 
 workbox.clientsClaim();
